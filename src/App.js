2dfdbfe9cae6d335a9ebd52ad4e70982c9560b9d
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2021-04-16 15:35:57 +0300

    pet-charity-fe-feature/registration - created initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
 
 const App = () => {
   return (
-    <>
-      <div>Pet Charity</div>
-    </>
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Route path="/" component={Landing} exact />
+        <Switch>
+          <Route path="/register" component={Register} exact />
+        </Switch>
+      </>
+    </BrowserRouter>
   );
 };
 
