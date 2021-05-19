9e37f9880acbae3c446be819dc595b120220b2f3
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2021-05-19 18:08:37 +0300

    pet-charity-fe-feature/modification-admin-panel - added query at url

--- src/components/shared/filter/Filter.js
+++ src/components/shared/filter/Filter.js
@@ -1,41 +1,42 @@
 import { Formik, Form, Field } from "formik";
 
 const Filter = ({ onFiltersSubmit, initialValues }) => {
-	const validate = () => {
-		const errors = {};
-		return errors;
-	};
+  const validate = () => {
+    const errors = {};
+    return errors;
+  };
 
-	const onSubmit = (values) => {
-		onFiltersSubmit(values);
-	};
+  const onSubmit = (values) => {
+    onFiltersSubmit(values);
+  };
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			validate={validate}
-			onSubmit={onSubmit}
-		>
-			{({ isSubmitting }) => (
-				<Form>
-					<Field name="type" as="select">
-						<option value="">all</option>
-						<option value="cat">cat</option>
-						<option value="dog">dog</option>
-						<option value="bird">bird</option>
-					</Field>
-					<Field as="select" name="sex">
-						<option value="">all</option>
-						<option value="he">male</option>
-						<option value="she">female</option>
-					</Field>
-					<button type="submit" disabled={isSubmitting}>
-						Find
-					</button>
-				</Form>
-			)}
-		</Formik>
-	);
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field name="type" as="select">
+            <option value="">all</option>
+            <option value="cat">cat</option>
+            <option value="dog">dog</option>
+            <option value="bird">bird</option>
+          </Field>
+          <Field as="select" name="sex">
+            <option value="">all</option>
+            <option value="he">male</option>
+            <option value="she">female</option>
+          </Field>
+          <button type="submit" disabled={isSubmitting}>
+            Find
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default Filter;
--- src/pages/adopt/components/Animals.js
+++ src/pages/adopt/components/Animals.js
@@ -5,87 +5,128 @@ import { Pagination } from "antd";
 import { getAnimals } from "../../../redux/thunks/animal";
 import { AnimalItem } from "./AnimalItem";
 import {
-	Container,
-	List,
-	Title,
+  Container,
+  List,
+  Title,
+  Btn,
 } from "../../../components/shared/styles/layout";
 
 import Spinner from "../../../components/shared/spinner/Spinner";
 import { colors } from "../../../components/shared/styles/global";
 import Filter from "../../../components/shared/filter/Filter";
+import { useHistory } from "react-router";
+import * as queryString from "querystring";
 
 const Animals = () => {
-	const [filterCriteria, setFilterCriteria] = useState({
-		currentPage: 1,
-		pageSize: 10,
-		sex: "",
-		type: "",
-	});
+  const [filterCriteria, setFilterCriteria] = useState({
+    currentPage: 1,
+    pageSize: 10,
+    type: "",
+    sex: "",
+  });
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-	const { currentPage, pageSize, sex, type } = filterCriteria;
-	const { animals, loading } = useSelector((state) => state.animal);
+  const { currentPage, pageSize, sex, type } = filterCriteria;
+  const { animals, loading } = useSelector((state) => state.animal);
 
-	const onPageChange = (page) => {
-		setFilterCriteria({ ...filterCriteria, currentPage: page });
-		// dispatch(getAnimals(page, pageSize, type, sex));
-	};
+  const onPageChange = (page) => {
+    setFilterCriteria({ ...filterCriteria, currentPage: page });
+    // dispatch(getAnimals(page, pageSize, type, sex));
+  };
 
-	const onFiltersSubmit = (props) => {
-		setFilterCriteria({ ...filterCriteria, type: props.type, sex: props.sex });
-		// dispatch(getAnimals(currentPage, pageSize, props.type, props.sex));
-	};
+  const onFiltersSubmit = (props) => {
+    setFilterCriteria({ ...filterCriteria, type: props.type, sex: props.sex });
+    // dispatch(getAnimals(1, pageSize, props.type, props.sex));
+  };
 
-	const onClear = () =>
-		setFilterCriteria({
-			currentPage: 1,
-			pageSize: 10,
-			sex: "",
-			type: "",
-		});
+  const onClear = () => {
+    setFilterCriteria({
+      currentPage: 1,
+      pageSize: 10,
+      sex: "",
+      type: "",
+    });
+    // dispatch(getAnimals(currentPage, pageSize, type, sex));
+  };
 
-	useEffect(() => {
-		dispatch(getAnimals(currentPage, pageSize, type, sex));
-	}, [dispatch, filterCriteria]);
+  useEffect(() => {
+    const { search } = history.location;
+    const parsed = queryString.parse(search.substr(1));
 
-	return (
-		<Container>
-			<Title color={colors.primary} textAlign="center">
-				Animals
-			</Title>
-			{loading ? (
-				<Spinner />
-			) : (
-				<>
-					<Filter
-						currentPage={currentPage}
-						pageSize={pageSize}
-						onFiltersSubmit={onFiltersSubmit}
-						initialValues={{ type, sex }}
-					/>
-					<List>
-						<Pagination
-							showQuickJumper
-							onChange={onPageChange}
-							defaultCurrent={1}
-							current={currentPage}
-							total={animals.count}
-						/>
-					</List>
-					<List>
-						{animals.length !== 0 ? (
-							animals.animals.map((animal) => (
-								<AnimalItem key={animal._id} animal={animal} />
-							))
-						) : (
-							<p>Animals not found. Please, Try to refine your criteria</p>
-						)}
-					</List>
-				</>
-			)}
-		</Container>
-	);
+    setFilterCriteria({
+      ...filterCriteria,
+      currentPage: Number(parsed.page),
+      type: parsed.type,
+      sex: parsed.sex,
+    });
+  }, []);
+
+  useEffect(() => {
+    // const query = {};
+
+    // if (filterCriteria.currentPage > 1) {
+    //   query.page = String(filterCriteria.currentPage);
+    // }
+
+    // if (filterCriteria.type) {
+    //   query.type = String(filterCriteria.type);
+    // }
+
+    // if (filterCriteria.sex) {
+    //   query.sex = String(filterCriteria.sex);
+    // }
+
+    // console.log(queryString.stringify(query));
+
+    history.push({
+      pathname: "/profile",
+      search: `?page=${currentPage}&type=${type}&sex=${sex}`,
+      // search: queryString.stringify(query),
+    });
+
+    dispatch(getAnimals(currentPage, pageSize, type, sex));
+  }, [dispatch, filterCriteria]);
+
+  return (
+    <Container>
+      <Title color={colors.primary} textAlign="center">
+        Animals
+      </Title>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Filter
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onFiltersSubmit={onFiltersSubmit}
+            initialValues={{ type, sex }}
+          />
+          <Btn onClick={onClear}>clear the filter</Btn>
+          <List>
+            <Pagination
+              showQuickJumper
+              onChange={onPageChange}
+              defaultCurrent={1}
+              current={currentPage}
+              total={animals.count}
+            />
+          </List>
+          <List>
+            {animals.length !== 0 ? (
+              animals.animals.map((animal) => (
+                <AnimalItem key={animal._id} animal={animal} />
+              ))
+            ) : (
+              <p>Animals not found. Please, Try to refine your criteria</p>
+            )}
+          </List>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Animals;
