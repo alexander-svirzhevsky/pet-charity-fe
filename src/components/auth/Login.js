17e0ddd547c0d7f1b6aa4e733cfdbea943a8557e
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2021-05-25 17:17:51 +0300

    pet-charity-fe-feature/signin-google - working on signin with google

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Form, Input } from "antd";
@@ -10,6 +10,41 @@ import RegisterImg from "../../assets/images/cat3.jpg";
 
 const Login = () => {
   const dispatch = useDispatch();
+
+  const insertGapiScript = () => {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/platform.js";
+    script.onload = () => {
+      initializeGoogleSignIn();
+    };
+
+    document.body.appendChild(script);
+  };
+
+  const initializeGoogleSignIn = () => {
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2.init({
+        client_id:
+          "685181765937-u21ssegohqb8k1urigi9i2vq81sbjlls.apps.googleusercontent.com",
+      });
+      console.log("api inited");
+
+      window.gapi.load("signin2", () => {
+        const params = {
+          onsuccess: () => {
+            console.log("user has finished signing in!");
+          },
+        };
+        window.gapi.signin2.render("loginButton", params);
+      });
+    });
+  };
+
+  useEffect(() => {
+    console.log("loading");
+
+    insertGapiScript();
+  }, []);
 
   const [formData, setFormData] = useState({
     email: "",
@@ -35,6 +70,7 @@ const Login = () => {
     <BackgroundFill image={RegisterImg}>
       <Container>
         <Title>Log in</Title>
+        <div id="loginButton">Sign in with Google</div>
         <Form
           name="basic"
           initialValues={{
